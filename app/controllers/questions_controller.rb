2358caa6165e8cc97528487f730609b792b58d3c
Bruno Passos <bgpassos@gmail.com>
2018-11-03 17:45:24 -0300

    Revision Stupid Coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,20 +1,22 @@
 class QuestionsController < ApplicationController
-
   def ask
   end
 
+  def answer
+    @question = params[:question]
+    @answer = coach_answer(params[:question])
+  end
+
   def coach_answer(question)
-    if question.downcase == "i am going to work right now!"
-    "Great!"
-    elsif question.end_with?("?")
-    "Silly question, get dressed and go to work!"
+    if question.downcase == "i am going to work" || question.downcase == "i'm going to work"
+      "Great!"
+    elsif question.include? "?"
+      "Silly question, get dressed and go to work!"
     else
-    "I don't care, get dressed and go to work!"
+      "I don't care, get dressed and go to work!"
     end
   end
-
-  def answer
-    @question = coach_answer(params[:question])
-  end
-
 end
+
+
+
